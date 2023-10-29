e98c7c43915b2b877e731682865cad1294fdeb01
Jung9928 <fg43irks@naver.com>
2023-10-29 20:57:56 +0900

    #6 - 스프링 부트 프로젝트 시작
    * spring web
    * actuator
    * dev-tools
    * lombok

---
+++ dashboard-project/src/main/java/com/jung9928/dashboardproject/DashboardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jung9928.dashboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardProjectApplication.class, args);
+    }
+
+}
