c125a1981b7856c3b311faf47a35ee31693fff14
Jung9928 <fg43irks@naver.com>
2023-11-12 13:49:53 +0900

    #1 - QueryDSL 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- dashboard-project/src/main/java/com/jung9928/dashboardproject/repository/ArticleCommentRepository.java
+++ dashboard-project/src/main/java/com/jung9928/dashboardproject/repository/ArticleCommentRepository.java
@@ -1,10 +1,30 @@
 package com.jung9928.dashboardproject.repository;
 
 import com.jung9928.dashboardproject.domain.ArticleComment;
+import com.jung9928.dashboardproject.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
 
+        // 제목, 내용, 해시태그, 작성일시, 작성자로 검색 가능하도록 함.
+        bindings.including(root.content, root.createdAt, root.createdBy);
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);    // 쿼리 생성 시, like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
